ccc3895cbf78a9418c93b761bbe9d8ddbb6ddba5
enea jorgo <eneajorgo1@gmail.com>
2024-12-16 16:36:18 +0100

    update

--- models/staging/staging_flights_one_month.sql
+++ models/staging/staging_flights_one_month.sql
@@ -1,7 +1,4 @@
 {{ config(materialized='view') }}
 
-WITH flights_one_month AS (
-    SELECT *
-    FROM {{ref('s_eneajorgo.flights_filtered')}}
-)
-SELECT * FROM flights_one_month
+SELECT *
+FROM {{ref('s_eneajorgo.flights_filtered')}}
